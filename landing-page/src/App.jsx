1ef9b32907b0a7c797de18bb6d2463588f728047
navia <naviamatute@yahoo.com>
2023-11-01 20:47:51 -0400

    nav bar 1st commit

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "./components/navigation/Navbar"
+
+function App()  {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
+ 
+
+  
+
---
+++ landing-page/src/components/navigation/Navbar.jsx
@@ -0,0 +1,18 @@
+import'./Navbar.css'
+
+
+export function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#home">Home</a>
+                    <a href="#faith">Faith</a>
+                    <a href="#fitness">Fitness</a>
+                    <a href="#finances">Finances</a>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
