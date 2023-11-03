c54bb64b1c10b8e3abd63bfbded9259d1d70f21a
navia <naviamatute@yahoo.com>
2023-11-03 11:12:59 -0400

    Design

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/navigation/Navbar"
+import { HeroSection } from "./components/HeroSection"
 
 function App()  {
   return (
     <>
     <Navbar/>
+    <HeroSection/>
     </>
   )
 }
--- landing-page/src/components/navigation/Navbar.jsx
+++ landing-page/src/components/navigation/Navbar.jsx
@@ -1,6 +1,7 @@
 import'./Navbar.css'
 
 
+
 export function Navbar() {
     return (
         <nav>
