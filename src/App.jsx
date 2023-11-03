df49bdbcf396880e76e25986c8504dcff9f68151
navia <naviamatute@yahoo.com>
2023-11-03 18:15:54 -0400

    savw

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from "./components/navigation/Navbar"
 import { HeroSection } from "./components/HeroSection"
+import { Footer } from "./components/footer/Footer"
 
 function App()  {
   return (
     <>
     <Navbar/>
     <HeroSection/>
+    <Footer/>
     </>
   )
 }
