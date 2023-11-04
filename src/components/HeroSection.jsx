92efe2597c2f458fb642c0b6a1f276586b213da0
navia <naviamatute@yahoo.com>
2023-11-03 20:52:43 -0400

    Done

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,11 @@
 import './HeroSection.css'
-import { Container, Card} from 'reactstrap';
+
 
 export function HeroSection() {
     return (
       <>
-        <Container id="container">
-          <Card id="onecard" style={{ width: "28rem", padding: "30px" }}>
+        <div id="container">
+          <div id="onecard" style={{ width: "28rem", padding: "30px" }}>
             <img
               id="faithpic"
               src="https://images.unsplash.com/photo-1602677416425-c84311bd217c?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -16,8 +16,8 @@ export function HeroSection() {
               thing that ever happened to me. I am excited to share my journey
               with you. I hope that this may bring you closer to him.
             </h1>
-          </Card>
-          <Card id="cardtwo"style={{ width: "28rem", padding: "30px"}}>
+          </div>
+          <div id="cardtwo"style={{ width: "28rem", padding: "30px"}}>
             <img
               id="workoutpic"
               src="https://images.unsplash.com/photo-1604603565810-9f2a167d6b6e?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTc0fHxmaXRuZXNzfGVufDB8fDB8fHww"
@@ -28,8 +28,8 @@ export function HeroSection() {
               reasearching about nutrition and fitness. Our bodies are a temple
               and we should watch what we eat and do with our bodies.
             </h1>
-          </Card>
-          <Card id="cardthree"style={{ width: "28rem", padding: "30px" }}>
+          </div>
+          <div id="cardthree"style={{ width: "28rem", padding: "30px" }}>
             <img
               id="freedompic"
               src="https://images.unsplash.com/photo-1511172459139-dc635d3274cd?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGJsb25kJTIwdHJhdmVsaW5nfGVufDB8fDB8fHww"
@@ -40,8 +40,8 @@ export function HeroSection() {
               debet in 5 months. I want to teach other women the same methods I
               used so they can achieve Financial Freedom as well!
             </h1>
-          </Card>
-        </Container>
+          </div>
+        </div>
       </>
     );
 }
